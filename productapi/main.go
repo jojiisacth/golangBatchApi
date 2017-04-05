200d3c090ec62e6b25987018efc51bab52ed6219
Joji <joji@Jojis-MacBook-Pro.local>
2017-04-05 10:30:02 +0530

    product api returning json products

--- productapi/main.go
+++ productapi/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 
+	"encoding/json"
+
 	"github.com/gorilla/mux"
 )
 
@@ -34,4 +36,25 @@ func getproducts(w http.ResponseWriter, r *http.Request) {
 	sb := string(body)
 	log.Println(sb)
 
+	products := make([]product, 6)
+
+	products[0] = product{"sweet mangoes", "1222", "mangoes of the time"}
+	products[1] = product{"greate grapes", "23455", "grapes of the time"}
+	products[2] = product{"greate grapes", "5523455", "grapes of the time"}
+	products[3] = product{"Tesco Gala Apple Minimum 5 Pack", "213441", "Tesco Gala Apple Minimum 5 Pack"}
+	products[4] = product{"Tesco Braeburn Apple Minimum 5 Pack 670G", "2344333", ""}
+	products[5] = product{"Tesco Apple Juice 1 Litre", "0987654444", ""}
+
+	//fmt.Fprintf(w, products)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	json.NewEncoder(w).Encode(products)
+	return
+
+}
+
+type product struct {
+	Name        string `json:"pname"`
+	Id          string `json:"id"`
+	Description string
 }
